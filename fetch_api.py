8960bcd8238dc218837204499f59fa6cb8b37d26
William Gomez <william.gomex@gmail.com>
2020-04-11 03:04:44 -0500

    feature/configuration: MVP configuration for analytics.

---
+++ fetch_api.py
@@ -0,0 +1,81 @@
+"""Hello Analytics Reporting API V4."""
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'service_account.json'
+VIEW_ID = '215611725'
+
+
+def initialize_analyticsreporting():
+    """Initializes an Analytics Reporting API V4 service object.
+
+    Returns:
+        An authorized Analytics Reporting API V4 service object.
+    """
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_FILE_LOCATION, SCOPES)
+
+    # Build the service object.
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+    return analytics
+
+
+def get_report(analytics):
+    """Queries the Analytics Reporting API V4.
+
+    Args:
+        analytics: An authorized Analytics Reporting API V4 service object.
+    Returns:
+        The Analytics Reporting API V4 response.
+    """
+    # Metrics and dimensions: https://ga-dev-tools.appspot.com/dimensions-metrics-explorer/
+    return analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+                {
+                    'viewId': VIEW_ID,
+                    'dateRanges': [{'startDate': '7daysAgo', 'endDate': 'today'}],
+                    'metrics': [{'expression': 'ga:users'}],
+                    'dimensions': [{'name': 'ga:userType'}]
+                }]
+        }
+    ).execute()
+
+
+def print_response(response):
+    """Parses and prints the Analytics Reporting API V4 response.
+
+    Args:
+        response: An Analytics Reporting API V4 response.
+    """
+    for report in response.get('reports', []):
+        columnHeader = report.get('columnHeader', {})
+        dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get(
+            'metricHeader', {}).get('metricHeaderEntries', [])
+
+        for row in report.get('data', {}).get('rows', []):
+            print(row)
+            dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
+
+            for header, dimension in zip(dimensionHeaders, dimensions):
+                print(header + ': ' + dimension)
+
+            for i, values in enumerate(dateRangeValues):
+                print('Date range: ' + str(i))
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    print(metricHeader.get('name') + ': ' + value)
+
+
+def main():
+    analytics = initialize_analyticsreporting()
+    response = get_report(analytics)
+    print_response(response)
+
+
+if __name__ == '__main__':
+    main()
